2f967911548063858ca0914133e3a810cde26507
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-08-24 20:39:10 -0600

    fixed chart labels

--- client/src/components/ui/chart.jsx
+++ client/src/components/ui/chart.jsx
@@ -142,11 +142,11 @@ const ChartTooltipContent = React.forwardRef((
     (<div
       ref={ref}
       className={cn(
-        "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+        "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-gray-800 text-white px-2.5 py-1.5 text-xs shadow-xl",
         className
       )}>
       {!nestLabel ? tooltipLabel : null}
-      <div className="grid gap-1.5">
+      <div className="grid  gap-1.5 text-white">
         {payload.map((item, index) => {
           const key = `${nameKey || item.name || item.dataKey || "value"}`
           const itemConfig = getPayloadConfigFromPayload(config, item, key)
@@ -156,7 +156,7 @@ const ChartTooltipContent = React.forwardRef((
             (<div
               key={item.dataKey}
               className={cn(
-                "flex w-full items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground",
+                "flex w-full items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-white",
                 indicator === "dot" && "items-center"
               )}>
               {formatter && (item?.value !== undefined) && item.name ? (
@@ -190,12 +190,12 @@ const ChartTooltipContent = React.forwardRef((
                     )}>
                     <div className="grid gap-1.5">
                       {nestLabel ? tooltipLabel : null}
-                      <span className="text-muted-foreground">
+                      <span className="text-white">
                         {itemConfig?.label || item.name}
                       </span>
                     </div>
                     {item.value && (
-                      <span className="font-mono font-medium tabular-nums text-foreground">
+                      <span className="font-mono font-medium tabular-nums text-white">
                         {item.value.toLocaleString()}
                       </span>
                     )}
