c21171b7df9a9cf4532266ad7cbf525b68f7b747
ewanchukwilliam <ewanchukwilliam@gmail.com>
2024-08-26 18:33:49 -0600

    fixed logout configuration

--- client/src/authentication/LogoutUser.jsx
+++ client/src/authentication/LogoutUser.jsx
@@ -1,12 +1,12 @@
 import {
-    ButtonTrigger,
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+  ButtonTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "../components/ui/dialog";
 import { Button } from "../components/ui/button";
 import useAuth from "./useAuth";
@@ -14,61 +14,65 @@ import { axiosInstance } from "./AuthProvider";
 import { useState } from "react";
 import { FaSignOutAlt } from "react-icons/fa";
 
-const LogoutUser = ({buttonProps,IconComponent}) => {
-	const { auth, setAuth } = useAuth();
-	const [logout, setLogout] = useState(false);
+const LogoutUser = ({ buttonProps, IconComponent }) => {
+  const { auth, setAuth } = useAuth();
+  const [logout, setLogout] = useState(false);
 
-	const handleClick = async (e) => {
-		e.preventDefault();
-		axiosInstance
-			.post("/user/logout")
-			.then((res) => {
-				if (res.status === 200) {
-					console.log("user was successfully logged out");
-					setLogout(true);
-					setAuth({ logout: true });
-				}
-			})
-			.catch((err) => {
-				if (err.response.status === 401) {
-					console.log("server responded that you have no cookie");
-				} else if (err.response.status === 500) {
-					console.log("something else went wrong", err);
-				}
-			});
-	};
+  const handleClick = async (e) => {
+    e.preventDefault();
+    axiosInstance
+      .post("/user/logout")
+      .then((res) => {
+        if (res.status === 200) {
+          console.log("user was successfully logged out");
+          setLogout(true);
+          setAuth({ logout: true });
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          console.log("server responded that you have no cookie");
+        } else if (err.response.status === 500) {
+          console.log("something else went wrong", err);
+        }
+      });
+  };
 
-	return (
-		<>
-			{logout && !auth?.user ? (
-				<>
-				</>
-			) : (
-				<Dialog>
+  return (
+    <>
+      {logout || !auth?.user ? (
+        <></>
+      ) : (
+        <Dialog>
           <ButtonTrigger
             buttonProps={buttonProps}
             IconComponent={IconComponent}
           >
             Sign Out
           </ButtonTrigger>
-					<DialogContent className="sm:max-w-[425px]">
-						<DialogHeader>
-							<DialogTitle>User Logout page</DialogTitle>
-							<DialogDescription>
-								Are you sure you want to logout?
-							</DialogDescription>
-						</DialogHeader>
-						<DialogFooter>
-							<Button type="submit" onClick={handleClick} variant="outline" className="flex gap-2 justify-center items-center text-center">
-									<FaSignOutAlt />
-								Logout
-							</Button>
-						</DialogFooter>
-					</DialogContent>
-				</Dialog>
-			)}
-		</>
-	);
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>User Logout page</DialogTitle>
+              <DialogDescription>
+                Are you sure you want to logout?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button
+                type="submit"
+                onClick={handleClick}
+                variant="outline"
+                className="flex gap-2 justify-center items-center text-center"
+              >
+                <FaSignOutAlt />
+                Logout
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
+  );
 };
 
 export default LogoutUser;
